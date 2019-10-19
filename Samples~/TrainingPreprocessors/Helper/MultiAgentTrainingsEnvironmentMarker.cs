c5b1354b0685e87fa7f51638e6fac8cd3480f240
Marcel Wiessler <marcel@gaisterhand.de>
2019-10-19 18:16:36 +0200

    * copyTarget Transform is by default hidden as it should always reside in the scene root and never be touched (only generated for training)

--- Samples~/TrainingPreprocessors/Helper/MultiAgentTrainingsEnvironmentMarker.cs
+++ Samples~/TrainingPreprocessors/Helper/MultiAgentTrainingsEnvironmentMarker.cs
@@ -12,12 +12,12 @@ namespace Helper
     public class MultiAgentTrainingsEnvironmentMarker : MonoBehaviour
     {
 #if UNITY_EDITOR
-        [SerializeField] private Transform Environment, CopyTarget;
+        [SerializeField] private Transform Environment;
         [SerializeField] private int CopyCount = 0;
 
         [HideInInspector]
         [SerializeField]
-        private Transform previousCopyTarget;
+        private Transform copyTarget;
 
         public int Count => CopyCount;
 
@@ -30,15 +30,13 @@ namespace Helper
             foreach (var rend in environmentRenderer)
                 totalBounds.Encapsulate(rend.bounds);
 
-            if (CopyTarget == null)
-                CopyTarget = new GameObject(Environment.name + "-Copies").transform;
-            previousCopyTarget = CopyTarget;
+            if (copyTarget == null) copyTarget = new GameObject(Environment.name + "-Copies").transform;
             DestroyCopies();
 
 //            var isPrefab = PrefabUtility.IsAnyPrefabInstanceRoot(Environment.gameObject);
             for (var i = 0; i < CopyCount; i++)
             {
-                var instance = Instantiate(Environment, CopyTarget, false);
+                var instance = Instantiate(Environment, copyTarget, false);
                 if (!instance || instance == null)
                 {
                     Debug.LogWarning("Failed to create Environment Instance for " + Environment.name, this);
@@ -50,50 +48,36 @@ namespace Helper
             }
             
             HideAndDisablePickingOfCopiedEnvironments();
-            Debug.Log("Created " + CopyTarget.childCount + " Training Copies of " + Environment.name, this);
+            Debug.Log("Created " + copyTarget.childCount + " Training Copies of " + Environment.name, this);
         }
 
         [ContextMenu(nameof(DestroyCopies))]
         public void DestroyCopies()
         {
-            if (previousCopyTarget == Environment) return;
-            if (previousCopyTarget == null) return;
-            var count = previousCopyTarget.childCount;
+            if (copyTarget == Environment) return;
+            if (copyTarget == null) return;
+            var count = copyTarget.childCount;
             if (count <= 0) return;
-            for (var i = previousCopyTarget.childCount - 1; i >= 0; i--)
+            for (var i = copyTarget.childCount - 1; i >= 0; i--)
             {
-                var child = previousCopyTarget.GetChild(i);
+                var child = copyTarget.GetChild(i);
                 if (Application.isPlaying)
                     Destroy(child.gameObject);
                 else DestroyImmediate(child.gameObject);
             }
             Debug.Log("Removed " + count + " Training Copies of " + Environment.name, this);
+            
+            if (Application.isPlaying)
+                Destroy(copyTarget.gameObject);
+            else DestroyImmediate(copyTarget.gameObject);
         }
 
         public void HideAndDisablePickingOfCopiedEnvironments()
         {
-            if (CopyTarget == null)
-            {
-                Debug.Log("No copy Target");
+            if (copyTarget == null)
                 return;
-            }
-            SceneVisibilityManager.instance.Hide(CopyTarget.gameObject, true);
-            SceneVisibilityManager.instance.DisablePicking(CopyTarget.gameObject, true);
-        }
-
-        // safety checks:
-        private void OnValidate()
-        {
-            if (previousCopyTarget != null && CopyTarget != previousCopyTarget)
-            {
-                DestroyCopies();
-            }
-
-            if (CopyTarget != null && Environment == CopyTarget)
-            {
-                Debug.LogWarning("Environment and CopyTarget are not allowed to be the same", this);
-                CopyTarget = null;
-            }
+            SceneVisibilityManager.instance.Hide(copyTarget.gameObject, true);
+            SceneVisibilityManager.instance.DisablePicking(copyTarget.gameObject, true);
         }
 
         private void OnEnable()
